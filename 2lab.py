568e4ff97256a416adb220e4990c150f968689cd
7Xron <76778683+7Xron@users.noreply.github.com>
2024-05-18 13:49:51 +0300

    Update 2lab.py

--- 2lab.py
+++ 2lab.py
@@ -7,14 +7,14 @@ G.add_node(0)
 G.add_node(1)
 G.add_node(2)
 
-G.add_node(3)
-G.add_node(4)
-
-
 # добавление ребр
 G.add_edge(0, 1)
 G.add_edge(1, 2)
 G.add_edge(2, 3)
+
+G = nx.cycle_graph(2)
+G.add_node(3)
+G.add_node(4)
 G.add_edge(3, 4)
 
 print(G)
