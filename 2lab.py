522c78fc2d1fbbda94000a4871597ed812a29683
7Xron <76778683+7Xron@users.noreply.github.com>
2024-05-18 12:37:03 +0300

    Update 2lab.py

--- 2lab.py
+++ 2lab.py
@@ -1,13 +1,17 @@
 import networkx as nx
 
 G = nx.Graph()
-G.add_node(0)
+
+# добавление узлов
+G.add_node(0) 
 G.add_node(1)
 G.add_node(2)
 
 G.add_node(3)
 G.add_node(4)
 
+
+# добавление ребр
 G.add_edge(0, 1)
 G.add_edge(1, 2)
 G.add_edge(2, 3)
